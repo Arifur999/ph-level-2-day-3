22aea9083ce97621275b9af10ee52f31119d71bb
Arifur999 <arr96777@gmail.com>
2025-11-03 21:28:04 +0600

    added class

--- closer.js
+++ closer.js
@@ -1,14 +1,31 @@
 //---------------------closer----------------------------
 
-const createCounter = () => {
-  let count = 0;
-  return (amount) => {
-    count = count + amount;
-    return count;
-  };
+// const createCounter = () => {
+//   let count = 0;
+//   return (amount) => {
+//     count = count + amount;
+//     return count;
+//   };
 
-};
-const counter=createCounter();
-console.log(counter(3));
-console.log(counter(2));
-//----------------------------------------------------------
+// };
+// const counter=createCounter();
+// console.log(counter(3));
+// console.log(counter(2));
+
+//--------------------------class add--------------------------------
+
+class Counter {
+  constructor(count) {
+    this.count = count;
+  }
+  add(amount) {
+    this.count = this.count + amount;
+  }
+  print() {
+    console.log(this.count);
+  }
+}
+
+const counter1 = new Counter(10);
+counter1.print();
+//-----------------------------------------------
--- state.js
+++ state.js
@@ -11,17 +11,17 @@
 
 //------OBJECT stateful------------------------
 
-const counter={
-    count:0,
-    add(amount){
-        this.count=this.count+amount;
+// const counter={
+//     count:0,
+//     add(amount){
+//         this.count=this.count+amount;
        
-    },
-     print(){
-            console.log(this.count);
-        },
-}
+//     },
+//      print(){
+//             console.log(this.count);
+//         },
+// }
 
-counter.add(2);
-counter.add(3);
-counter.print();
+// counter.add(2);
+// counter.add(3);
+// counter.print();
