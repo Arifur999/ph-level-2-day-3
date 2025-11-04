3fff19b429ef651fb7788c8e85a69f2f30fa8478
Arifur999 <arr96777@gmail.com>
2025-11-04 19:42:27 +0600

    added link list append

---
+++ advanceLinkList.js
@@ -0,0 +1,53 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    //if the link list is empty
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      //if the link list is not empty
+
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+  }
+
+//   prepend() {}
+
+//   insert() {}
+
+//   remove() {}
+
+  print() {
+    const arr=[]
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      arr.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(arr.join("->"),"-> null");
+  }
+}
+
+const linklist = new LinkList();
+linklist.append(1);
+linklist.append(2);
+linklist.append(3);
+
+linklist.print();
